0377e7391c7a44358bd66cd93cfacd12fce2acd4
Sergio Martín <giosoftware@gmail.com>
2019-01-21 11:56:15 +0100

    Routes to home, login, register, user and not found component were added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { NotfoundComponent } from './commons/notfound/notfound.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { GuardService } from './services/guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, canActivate: [GuardService] },
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'user', component: UserComponent, canActivate: [GuardService] },
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
