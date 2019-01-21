ec4fc33486feeb15a56d72755128858de16c8d1a
Sergio Martín <giosoftware@gmail.com>
2019-01-21 11:57:33 +0100

    ReactiveFormsModule, HttpClientModule and all components have been imported.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonsModule } from './commons/commons.module';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    UserComponent,
+    LoginComponent,
+    HomeComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RegisterComponent, UserComponent, LoginComponent, HomeComponent, PostComponent]
 })
 export class AppModule { }
