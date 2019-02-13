e60848a95f8093c2b87aea7acfe5c104ebc31b17
Sergio Martín <giosoftware@gmail.com>
2019-02-13 12:06:18 +0100

    If we create HttpHeaders outside the class, a just login user get last loginned user wall. Fixed!

--- src/app/services/wall.service.ts
+++ src/app/services/wall.service.ts
@@ -2,13 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + window.localStorage.getItem(environment.LSTOKEN)
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -16,7 +9,13 @@ export class WallService {
 
   constructor(private http: HttpClient) { }
 
-  getWall(month) {
+  getWall(month: String) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem(environment.LSTOKEN)
+      })
+    };
     return this.http.get(environment.API_URL + 'wall/' + month, httpOptions);
   }
 }
