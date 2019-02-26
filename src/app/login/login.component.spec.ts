49e9e94acc88ea4cc18e88b849f8a86c5d53403a
Sergio Martín <giosoftware@gmail.com>
2019-02-26 12:46:41 +0100

    Is mandatory to use NO_ERRORS_SCHEMA

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 
 import { LoginComponent } from './login.component';
 import { AuthenticationService } from '../services/authentication.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 
 /**
@@ -26,7 +27,8 @@ describe('LoginComponent (shallow test)', () => {
 
     TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule],
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents(); // This is not needed if you are in the CLI context
   }));
 
